c66ef8af8c1dc0db56717e0d06d9841229c82501
Rafael <rafaelg.motta@hotmail.com>
2023-10-04 17:07:27 -0300

    Json att

--- Menu/script.js
+++ Menu/script.js
@@ -32,3 +32,62 @@ function buttonClick() {
     });
   }
   
+  function fillProductBox(product, productBox) {
+    const productImageDiv = document.createElement("div");
+    productImageDiv.classList.add("product-image");
+  
+    const productImage = document.createElement("img");
+    productImage.src = product.imagePath;
+    productImage.alt = "Foto do produto";
+  
+    productImageDiv.appendChild(productImage);
+  
+    const productInfoDiv = document.createElement("div");
+    productInfoDiv.classList.add("product-info");
+  
+    const productName = document.createElement("p");
+    productName.classList.add("product-name");
+    productName.textContent = product.name;
+  
+    const productDescription = document.createElement("p");
+    productDescription.classList.add("product-description");
+    productDescription.textContent = product.description;
+  
+    productInfoDiv.appendChild(productName);
+    productInfoDiv.appendChild(productDescription);
+  
+    const productPriceDiv = document.createElement("div");
+    productPriceDiv.classList.add("price");
+  
+    const productPrice = document.createElement("p");
+    productPrice.textContent = `R$ ${product.value.toFixed(2)}`;
+  
+    const buyButton = document.createElement("button");
+    buyButton.classList.add("orange-button");
+    buyButton.textContent = "Comprar";
+  
+    productPriceDiv.appendChild(productPrice);
+    productPriceDiv.appendChild(buyButton);
+  
+    productBox.appendChild(productImageDiv);
+    productBox.appendChild(productInfoDiv);
+    productBox.appendChild(productPriceDiv);
+  }
+  
+  fetch("products.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const productsContainer = document.querySelector(".products");
+  
+      data.forEach((product) => {
+        const productBox = document.createElement("div");
+        productBox.classList.add("product-box");
+        fillProductBox(product, productBox);
+        productsContainer.appendChild(productBox);
+      });
+    })
+    .catch((error) => {
+      console.log("Erro ao carregar o arquivo JSON:", error);
+    });
+
+   
