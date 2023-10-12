694013d45ee566c11bc26b430fe640d21addbc25
Rafael <rafaelg.motta@hotmail.com>
2023-10-11 23:54:55 -0300

    login page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
 import Image from "next/image.js";
 import * as styles from "./styles.js";
 import foodImage from "../assets/images/food.jpg";
+import messageImage from "../assets/images/Message.jpg";
+import passwordImage from "../assets/images/Password.jpg";
+import googleImage from "../assets/images/Google.jpg";
 
 export default function Login() {
   return (
@@ -19,9 +22,40 @@ export default function Login() {
       <styles.Content>
         <styles.Title>Bem vindo!</styles.Title>
         <styles.Subtitle>faça o login para continuar</styles.Subtitle>
-        <styles.Input placeholder="Seu e-mail" />
-        <styles.Input placeholder="senha" />
+
+        <styles.InputContainer>
+          <Image src={messageImage} alt="Ícone mensagem" />
+          <styles.CustomInput type="text" placeholder="Seu e-mail" />
+        </styles.InputContainer>
+
+        <styles.InputContainer>
+          <Image src={passwordImage} alt="Ícone senha" />
+          <styles.CustomInput type="password" placeholder="senha" />
+        </styles.InputContainer>
+
         <styles.Button>Entrar</styles.Button>
+
+        <styles.LineContainer>
+          <styles.Line />
+          <styles.LineText>ou</styles.LineText>
+          <styles.Line />
+        </styles.LineContainer>
+
+        <styles.GoogleButton>
+          <Image
+            src={googleImage}
+            style={{
+              display: "flex",
+            }}
+            alt="Google"
+          />
+          <styles.GoogleButtonText>Entrar com o Google</styles.GoogleButtonText>
+        </styles.GoogleButton>
+
+        <styles.RegisterContainer>
+          <styles.Subtitle>Não tem uma conta?</styles.Subtitle>
+          <styles.RegisterLink>Cadastre</styles.RegisterLink>
+        </styles.RegisterContainer>
       </styles.Content>
     </styles.Container>
   );
--- src/pages/styles.js
+++ src/pages/styles.js
@@ -44,15 +44,36 @@ export const Subtitle = styled.p`
   color: #9098b1;
 `;
 
-export const Input = styled.input`
+export const InputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 25px;
   width: 343px;
   height: 48px;
-  padding: 12px, 16px, 12px, 16px;
+  padding: 0 16px;
   border-radius: 5px;
-  border: 1px;
-  border-color: #ebf0ff;
+  border: 1px solid #ebf0ff;
   gap: 10px;
-  margin-top: 25px;
+`;
+
+export const Icon = styled.img`
+  width: 20px;
+  height: 20px;
+  margin-right: 10px;
+`;
+
+export const CustomInput = styled.input`
+  flex: 1;
+  border: none;
+  outline: none;
+  height: 100%;
+  width: 100%;
+  font-family: Poppins;
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 22px;
+  letter-spacing: 0.5px;
+  text-align: left;
 `;
 
 export const Button = styled.button`
@@ -73,4 +94,75 @@ export const Button = styled.button`
   letter-spacing: 0.5px;
   text-align: center;
   color: #ffffff;
+`;
+
+export const LineContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 25px;
+`;
+
+export const Line = styled.span`
+  flex: 1;
+  height: 1px;
+  width: 135px;
+  top: 355px;
+  border: 1px solid #ebf0ff;
+`;
+
+export const LineText = styled.span`
+  font-family: Poppins;
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 21px;
+  letter-spacing: 0.005em;
+  color: #9098b1;
+  padding: 0 10px;
+  margin: 0px 13px 0px 13px;
+`;
+
+export const GoogleButton = styled.button`
+  display: flex;
+  align-items: center;
+  align-content: center;
+  justify-content: center;
+  width: 343px;
+  height: 57px;
+  padding: 16px;
+  border-radius: 5px;
+  gap: 10px;
+  cursor: pointer;
+  border: 1px solid #ebf0ff;
+  background: transparent;
+  margin-top: 25px;
+`;
+
+export const GoogleButtonText = styled.p`
+  flex: 1;
+  font-family: Poppins;
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 25px;
+  letter-spacing: 0.5px;
+  text-align: center;
+  color: #9098b1;
+`;
+
+export const RegisterContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 15px;
+`;
+
+export const RegisterLink = styled.a`
+  margin-left: 5px;
+  cursor: pointer;
+
+  font-family: Poppins;
+  font-size: 12px;
+  font-weight: 700;
+  line-height: 18px;
+  letter-spacing: 0.5px;
+  text-align: center;
+  color: #40bfff;
 `;
